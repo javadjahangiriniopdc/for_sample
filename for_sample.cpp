06b4d523054bb3cfde0abac1837295cb8beaf6f3
javajahangiriniopdc <javad.jahangiri.niopdc@gmail.com>
2020-06-18 16:32:51 +0430

    add jadval zarb

--- for_sample.cpp
+++ for_sample.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main()
 {
-   
-    /*for (int i = 1;i <= 10;i++) {
+
+    for (int i = 1;i <= 10;i++) {
         for (int j = 1;j <= 10;j++)
             cout << i * j<<"\t";
 
         cout << endl;
 
-    }*/
+    }
 
-   
+  
 
     for (int i = 5;i >= 1;i--) {
         for (int j = 1;j <= i;j++)
